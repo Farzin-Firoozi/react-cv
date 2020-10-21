0e1bfd15857d87aa277d6cee150c39592bbd8cb9
Farzin Firoozi <farzin7813@gmail.com>
2020-10-21 15:46:22 +0330

    Updates

---
+++ precache-manifest.a3e8bb5a7db49266157a5889aaab9a16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d92b1c979f567bf13f322c61cb7286f",
+    "url": "/farzin-firoozi.github.io/react-cv/index.html"
+  },
+  {
+    "revision": "88288d8747a125a3dd2c",
+    "url": "/farzin-firoozi.github.io/react-cv/static/css/main.f3dd4cc9.chunk.css"
+  },
+  {
+    "revision": "f1dabfba40327a054e3b",
+    "url": "/farzin-firoozi.github.io/react-cv/static/js/2.b8017117.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/farzin-firoozi.github.io/react-cv/static/js/2.b8017117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88288d8747a125a3dd2c",
+    "url": "/farzin-firoozi.github.io/react-cv/static/js/main.0007609b.chunk.js"
+  },
+  {
+    "revision": "eeaf6f9fd375ffbe802a",
+    "url": "/farzin-firoozi.github.io/react-cv/static/js/runtime-main.f1294523.js"
+  }
+]);
